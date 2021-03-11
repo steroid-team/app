780ca294dcbf867b05c1f7985f82b866710e71b6
Vincent <vincentpellenc@gmail.com>
2021-03-11 20:57:53 +0100

    add tests for ItemViewActivity

--- app/src/androidTest/java/com/github/steroidteam/todolist/MainActivityTest.java
+++ app/src/androidTest/java/com/github/steroidteam/todolist/MainActivityTest.java
@@ -1,27 +1,16 @@
 package com.github.steroidteam.todolist;
 
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.intent.matcher.IntentMatchers;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.clearText;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
-
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
-
-    //private static final String TEST_NAME = "Alice";
 
     @Rule
     public ActivityScenarioRule<MainActivity> testRule = new ActivityScenarioRule<>(MainActivity.class);

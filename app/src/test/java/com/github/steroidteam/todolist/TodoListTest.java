b7f802c4d2d0913d6c5d26482fd71dc4194a58eb
Théo Damiani <theo.damiani@epfl.ch>
2021-03-12 23:08:22 +0100

    Implementation of the volatile database and test for database and todoList

--- app/src/test/java/com/github/steroidteam/todolist/TodoListTest.java
+++ app/src/test/java/com/github/steroidteam/todolist/TodoListTest.java
@@ -17,15 +17,26 @@ public class TodoListTest {
 
         Task t1 = new Task("body1");
         Task t2 = new Task("body2");
-        t2.setId(98);
+        t2.setIndex(98);
         TodoList td = new TodoList("This is a title!");
+
         td.addTask(t1);
         assertEquals(1, td.getSize());
+
+        td.removeTask(1);
+        td.removeTask(-1);
+        assertEquals(1, td.getSize());
+
         td.removeTask(0);
         assertEquals(0, td.getSize());
         assertTrue(td.isEmpty());
+
+        assertNull(td.getTask(0));
+        assertNull(td.getTask(-3));
+
         Integer hash = td.getDate().toString().hashCode();
         assertEquals(hash, td.getId());
+
         String newTitle = "new title !";
         td.setTitle(newTitle);
         assertEquals(newTitle, td.getTitle());
--- app/src/test/java/com/github/steroidteam/todolist/VolatileDatabaseTest.java
+++ app/src/test/java/com/github/steroidteam/todolist/VolatileDatabaseTest.java
@@ -2,6 +2,8 @@ package com.github.steroidteam.todolist;
 
 import com.github.steroidteam.todolist.database.DatabaseException;
 import com.github.steroidteam.todolist.database.VolatileDatabase;
+import com.github.steroidteam.todolist.todo.Task;
+import com.github.steroidteam.todolist.todo.TodoList;
 
 import org.junit.Test;
 
@@ -10,7 +12,117 @@ import static org.junit.Assert.*;
 public class VolatileDatabaseTest {
 
     @Test
-    public void someSimpleTest() {
+    public void databaseExceptionOk() {
+        assertThrows(DatabaseException.class, () -> {
+            throw new DatabaseException("This is a exception!");
+        });
+    }
 
+    @Test
+    public void todoWorks() {
+        TodoList todo = new TodoList("A title!");
+        VolatileDatabase database = new VolatileDatabase();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.putTodoList(null);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.removeTodoList(null);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.getTodoList(null);
+        });
+
+        database.putTodoList(todo);
+        assertEquals(todo, database.getTodoList(todo.getId()));
+
+        database.removeTodoList(todo.getId());
+        assertNull(database.getTodoList(todo.getId()));
+    }
+
+    @Test
+    public void putTaskWorks() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.putTask(null, new Task("Body!"));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.putTask(0, null);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            Task t = new Task("body!");
+            t.setIndex(1);
+            database2.putTask(0, t);
+        });
+
+        TodoList todo = new TodoList("A title!");
+        Task task = new Task("A body!");
+        VolatileDatabase database = new VolatileDatabase();
+
+        database.putTask(0, task);
+        assertNull(todo.getTask(0));
+
+        database.putTodoList(todo);
+        database.putTask(todo.getId(), task);
+        assertEquals(todo.getTask(0), task);
+    }
+
+    @Test
+    public void removeTaskWorks() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.removeTask(null, 0);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.removeTask(0, null);
+        });
+
+        TodoList todo = new TodoList("A title!");
+        Task task = new Task("A body!");
+        VolatileDatabase database = new VolatileDatabase();
+        database.putTodoList(todo);
+        database.putTask(todo.getId(), task);
+
+        database.removeTask(todo.getId(), 2);
+        assertEquals(1, todo.getSize());
+
+        database.removeTask(0, 0);
+        assertEquals(1, todo.getSize());
+
+        database.removeTask(todo.getId(), 0);
+        assertEquals(0, todo.getSize());
+    }
+
+    @Test
+    public void getTaskWorks() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.getTask(null, 0);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.getTask(0, null);
+        });
+
+        TodoList todo = new TodoList("A title!");
+        Task task = new Task("A body!");
+        VolatileDatabase database = new VolatileDatabase();
+        database.putTodoList(todo);
+        database.putTask(todo.getId(), task);
+
+        assertEquals(null, database.getTask(0, task.getIndex()));
+        assertEquals(task, database.getTask(todo.getId(), task.getIndex()));
     }
 }

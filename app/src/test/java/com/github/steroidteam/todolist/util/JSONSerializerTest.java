40f7e660dac71a51b7a88c556ce9f14d306ed9bf
Yago González <yagogonzalezg@gmail.com>
2021-03-18 19:23:51 +0100

    fix: Increase JSONSerializer time precision to ms
    With the current settings, two objects with the exact same dates could
    produce non-equal Date objects because of the milliseconds not being
    added to the serialized object.
    
    Explicitly including their values in the serialized string removes any
    uncertainty regarding the ms field of the dates.

--- app/src/test/java/com/github/steroidteam/todolist/util/JSONSerializerTest.java
+++ app/src/test/java/com/github/steroidteam/todolist/util/JSONSerializerTest.java
@@ -2,12 +2,11 @@ package com.github.steroidteam.todolist.util;
 
 import com.github.steroidteam.todolist.todo.Task;
 import com.github.steroidteam.todolist.todo.TodoList;
-import com.github.steroidteam.todolist.util.JSONSerializer;
 import com.google.gson.JsonSyntaxException;
 
 import org.junit.Test;
 
-import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
@@ -15,6 +14,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class JSONSerializerTest {
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
     @Test
     public void todoListSerializerRejectsNull() {
         assertThrows(NullPointerException.class, () -> {
@@ -34,8 +35,7 @@ public class JSONSerializerTest {
         final String LIST_TITLE = "Test list";
 
         final TodoList todoList = new TodoList(LIST_TITLE);
-        final String expectedFormattedDate = DateFormat.getDateTimeInstance(DateFormat.FULL,
-                DateFormat.FULL).format(todoList.getDate());
+        final String expectedFormattedDate = DATE_FORMAT.format(todoList.getDate());
 
         final String actualSerializedObject = JSONSerializer.serializeTodoList(todoList);
         final String expectedSerializedObject = "{" +
@@ -56,8 +56,7 @@ public class JSONSerializerTest {
         final TodoList todoList = new TodoList(LIST_TITLE);
         todoList.addTask(new Task(TASK1_DESC));
         todoList.addTask(new Task(TASK2_DESC));
-        final String expectedFormattedDate = DateFormat.getDateTimeInstance(DateFormat.FULL,
-                DateFormat.FULL).format(todoList.getDate());
+        final String expectedFormattedDate = DATE_FORMAT.format(todoList.getDate());
 
         final String actualSerializedObject = JSONSerializer.serializeTodoList(todoList);
         final String expectedSerializedObject = "{" +
@@ -73,9 +72,7 @@ public class JSONSerializerTest {
     public void todoListDeserializerWorksWithEmptyTasks() {
         final UUID FIXTURE_UUID = UUID.randomUUID();
         final String FIXTURE_TITLE = "Test list";
-        final DateFormat FIXTURE_DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.FULL,
-                DateFormat.FULL);
-        final String FIXTURE_FORMATTED_DATE = FIXTURE_DATE_FORMAT.format(new Date());
+        final String FIXTURE_FORMATTED_DATE = DATE_FORMAT.format(new Date());
 
         final String FIXTURE_JSON = "{" +
                 "\"id\":\"" + FIXTURE_UUID.toString() + "\"," +
@@ -88,7 +85,7 @@ public class JSONSerializerTest {
         assertEquals(actualDeserializedObject.getId(), FIXTURE_UUID);
         assertEquals(actualDeserializedObject.getSize(), 0);
         assertEquals(actualDeserializedObject.getTitle(), FIXTURE_TITLE);
-        assertEquals(FIXTURE_DATE_FORMAT.format(actualDeserializedObject.getDate()), FIXTURE_FORMATTED_DATE);
+        assertEquals(DATE_FORMAT.format(actualDeserializedObject.getDate()), FIXTURE_FORMATTED_DATE);
     }
 
     @Test
@@ -97,9 +94,7 @@ public class JSONSerializerTest {
         final String FIXTURE_TITLE = "Test list";
         final String TASK1_DESC = "Buy bananas";
         final String TASK2_DESC = "Eat fruit";
-        final DateFormat FIXTURE_DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.FULL,
-                DateFormat.FULL);
-        final String FIXTURE_FORMATTED_DATE = FIXTURE_DATE_FORMAT.format(new Date());
+        final String FIXTURE_FORMATTED_DATE = DATE_FORMAT.format(new Date());
 
         final String FIXTURE_JSON = "{" +
                 "\"id\":\"" + FIXTURE_UUID.toString() + "\"," +
@@ -114,7 +109,7 @@ public class JSONSerializerTest {
         assertEquals(actualDeserializedObject.getTask(0), new Task(TASK1_DESC));
         assertEquals(actualDeserializedObject.getTask(1), new Task(TASK2_DESC));
         assertEquals(actualDeserializedObject.getTitle(), FIXTURE_TITLE);
-        assertEquals(FIXTURE_DATE_FORMAT.format(actualDeserializedObject.getDate()), FIXTURE_FORMATTED_DATE);
+        assertEquals(DATE_FORMAT.format(actualDeserializedObject.getDate()), FIXTURE_FORMATTED_DATE);
     }
 
     @Test

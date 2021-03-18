4f7118677b57bcdd28bd004aa4567b7b1a1d5dad
Théo Damiani <theo.damiani@epfl.ch>
2021-03-18 13:22:07 +0100

    view: refactor the item view to use todo lists

--- app/src/main/java/com/github/steroidteam/todolist/ItemViewActivity.java
+++ app/src/main/java/com/github/steroidteam/todolist/ItemViewActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,7 +11,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -21,11 +20,9 @@ import android.widget.ListView;
 import com.github.steroidteam.todolist.todo.Task;
 import com.github.steroidteam.todolist.todo.TodoList;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class ItemViewActivity extends AppCompatActivity {
-    private static TasksAdapter adapter;
+    private static todoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +38,6 @@ public class ItemViewActivity extends AppCompatActivity {
         setTitle(todoList.getTitle());
 
         // Pre-populate the database with a few sample tasks.
-        List<Task> tasks = new ArrayList<>();
         todoList.addTask(new Task("Change passwords"));
         todoList.addTask(new Task("Replace old server"));
         todoList.addTask(new Task("Set up firewall"));
@@ -50,12 +46,13 @@ public class ItemViewActivity extends AppCompatActivity {
         todoList.addTask(new Task("Replace old server"));
         todoList.addTask(new Task("Set up firewall"));
 
-        adapter = new TasksAdapter(this, tasks);
+        //adapter = new TasksAdapter(this, tasks);
+        adapter = new todoAdapter(todoList);
         ListView listView = findViewById(R.id.activity_itemview_itemlist);
-        setListViewSettings(listView, tasks);
+        setListViewSettings(listView, todoList);
     }
 
-    private void setListViewSettings(ListView listView, List<Task> tasks) {
+    private void setListViewSettings(ListView listView, TodoList todoList) {
         listView.setAdapter(adapter);
 
         listView.setLongClickable(true);
@@ -63,8 +60,7 @@ public class ItemViewActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Task t = tasks.get(i);
-                tasks.remove(i);
+                todoList.removeTask(i);
                 adapter.notifyDataSetChanged();
                 return true;
             }
@@ -90,22 +86,42 @@ public class ItemViewActivity extends AppCompatActivity {
     }
 
 
-    private class TasksAdapter extends ArrayAdapter<Task> {
-        public TasksAdapter(Context context, List<Task> users) {
-            super(context, 0, users);
+    private class todoAdapter extends BaseAdapter {
+
+        private final TodoList todoList;
+
+        public todoAdapter(TodoList todoList) {
+            this.todoList = todoList;
+        }
+
+        public void add(Task task) {
+            todoList.addTask(task);
+        }
+
+        @Override
+        public int getCount() {
+            return todoList.getSize();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return todoList.getTask(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position; //No need to specify a particular ID, we just return the position.
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            Task task = getItem(position);
-
+            Task task = (Task)getItem(position);
             // Check if an existing view is being reused, otherwise inflate the view
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_task_item, parent, false);
-            }
 
+            convertView = LayoutInflater.from(getBaseContext()).inflate(R.layout.layout_task_item, parent, false);
             CheckBox taskView = convertView.findViewById(R.id.layout_task_checkbox);
             taskView.setText(task.getBody());
+
             taskView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -116,7 +132,7 @@ public class ItemViewActivity extends AppCompatActivity {
             taskView.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View view) {
-
+                    // Notify database?
                     return false;
                 }
             });

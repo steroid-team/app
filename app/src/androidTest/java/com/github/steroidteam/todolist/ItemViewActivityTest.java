feebd515e831b0cbda8abd52c5f4d66ef56da25b
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2021-03-18 19:01:32 +0100

    create ToastMatcher class to fix test for Toast message

--- app/src/androidTest/java/com/github/steroidteam/todolist/ItemViewActivityTest.java
+++ app/src/androidTest/java/com/github/steroidteam/todolist/ItemViewActivityTest.java
@@ -133,10 +133,33 @@ public class ItemViewActivityTest {
 
             onView(withText("Yes")).perform(click());
 
-            onView(withText("Successfully removed the task : Change passwords")).check(matches(isDisplayed()));
+            //onView(withText("Successfully removed the task : Change passwords")).check(matches(isDisplayed()));
+            onView(withText("Successfully removed the task : Change passwords")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
             //onView(withText("Successfully removed the task : Change passwords")).check(matches(isDisplayed()));
         }
     }
 
+
+    public class ToastMatcher extends TypeSafeMatcher<Root> {
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("is toast");
+        }
+
+        @Override
+        public boolean matchesSafely(Root root) {
+            int type = root.getWindowLayoutParams().get().type;
+            if (type == WindowManager.LayoutParams.TYPE_TOAST) {
+                IBinder windowToken = root.getDecorView().getWindowToken();
+                IBinder appToken = root.getDecorView().getApplicationWindowToken();
+                if (windowToken == appToken) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
 
 }

e965108207e5812c9efbe53066c3e5f66d88acb6
lgraziano <leandro.graziano@epfl.ch>
2021-03-25 14:38:10 +0100

    view: set up list creation

--- app/src/main/java/com/github/steroidteam/todolist/ListSelectionActivity.java
+++ app/src/main/java/com/github/steroidteam/todolist/ListSelectionActivity.java
@@ -1,21 +1,27 @@
 package com.github.steroidteam.todolist;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.TypedValue;
 import android.view.View;
-
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import com.firebase.ui.auth.AuthUI;
+import com.github.steroidteam.todolist.database.VolatileDatabase;
+import com.github.steroidteam.todolist.todo.TodoList;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 public class ListSelectionActivity extends AppCompatActivity {
+    private VolatileDatabase database = new VolatileDatabase();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +38,48 @@ public class ListSelectionActivity extends AppCompatActivity {
         AuthUI.getInstance().signOut(this).addOnCompleteListener(task -> thisActivity.finish());
     }
 
-    public void openList(View view){
+    public void openExampleList(View view){
         Intent itemViewActivity = new Intent(ListSelectionActivity.this, ItemViewActivity.class);
         startActivity(itemViewActivity);
+    }
+
+    public void openList(View view, TodoList list){
+        Intent itemViewActivity = new Intent(ListSelectionActivity.this, ItemViewActivity.class);
+        startActivity(itemViewActivity);
+    }
+
+    private void addList(TodoList list){
+        LinearLayout grid = findViewById(R.id.lists_layout);
+        TextView newList = new TextView(this);
+        newList.setText(list.getTitle());
+        float height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
+        newList.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) height));
+        newList.setTextAppearance(R.style.TextAppearance_AppCompat_Large);
+        newList.setOnClickListener((View view) ->{
+            openList(view, list);
+        });
+        grid.addView(newList);
+    }
+
+    public void createButton(View view){
+        AlertDialog.Builder titlePopup = new AlertDialog.Builder(this);
+        titlePopup.setTitle("Enter the title of your to-do list");
+        final EditText titleInput = new EditText(this);
+        titleInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        titlePopup.setView(titleInput);
+        titlePopup.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                TodoList newList = new TodoList(titleInput.getText().toString());
+                addList(newList);
+            }
+        });
+        titlePopup.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        titlePopup.show();
     }
 }

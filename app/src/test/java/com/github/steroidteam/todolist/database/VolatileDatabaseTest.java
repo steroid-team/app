4d32d788767740e850a9aab6f84f642202fd7922
Vincent <vincentpellenc@gmail.com>
2021-03-25 18:05:51 +0100

    add tests to the VolatileDatabase for notes

--- app/src/test/java/com/github/steroidteam/todolist/database/VolatileDatabaseTest.java
+++ app/src/test/java/com/github/steroidteam/todolist/database/VolatileDatabaseTest.java
@@ -2,6 +2,7 @@ package com.github.steroidteam.todolist.database;
 
 import com.github.steroidteam.todolist.database.DatabaseException;
 import com.github.steroidteam.todolist.database.VolatileDatabase;
+import com.github.steroidteam.todolist.notes.Note;
 import com.github.steroidteam.todolist.todo.Task;
 import com.github.steroidteam.todolist.todo.TodoList;
 
@@ -132,4 +133,34 @@ public class VolatileDatabaseTest {
         // Getting existent tasks from an existing list returns the corresponding Task.
         assertEquals(task, database.getTask(todo.getId(), 0));
     }
+
+    @Test
+    public void noteWorks() {
+        Note note = new Note("A title!");
+        VolatileDatabase database = new VolatileDatabase();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.putNote(null);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.removeNote(null);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            VolatileDatabase database2 = new VolatileDatabase();
+            database2.getNote(null);
+        });
+
+        database.putNote(note);
+        assertEquals(note, database.getNote(note.getId()));
+
+        database.removeNote(note.getId());
+        assertNull(database.getTodoList(note.getId()));
+
+
+    }
+
 }

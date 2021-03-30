05efbfb51b0468cba024aae9b6c33e3be95e1e13
Yago González <yagogonzalezg@gmail.com>
2021-03-30 12:23:32 +0200

    views: Remove useless ItemView toolbar

--- app/src/main/java/com/github/steroidteam/todolist/view/ItemViewActivity.java
+++ app/src/main/java/com/github/steroidteam/todolist/view/ItemViewActivity.java
@@ -6,14 +6,14 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.appcompat.app.ActionBar;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.github.steroidteam.todolist.R;
 import com.github.steroidteam.todolist.view.adapter.TodoAdapter;
 import com.github.steroidteam.todolist.viewmodel.ItemViewModel;
+
 import java.util.UUID;
 
 public class ItemViewActivity extends AppCompatActivity {
@@ -26,11 +26,6 @@ public class ItemViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_view);
-
-        Toolbar toolbar = findViewById(R.id.activity_itemview_toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
 
         // Instantiate the view model.
         // random UUID because we don't have persistent memory !

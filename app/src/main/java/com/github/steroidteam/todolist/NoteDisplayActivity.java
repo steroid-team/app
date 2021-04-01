7bacb8fd062b47784fd05ddc5b5d94f5d98bba06
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2021-04-01 19:28:34 +0200

    fix last commit

--- app/src/main/java/com/github/steroidteam/todolist/NoteDisplayActivity.java
+++ app/src/main/java/com/github/steroidteam/todolist/NoteDisplayActivity.java
@@ -2,6 +2,9 @@ package com.github.steroidteam.todolist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.appcompat.widget.Toolbar;
 import java.util.UUID;
 
 public class NoteDisplayActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +35,30 @@ public class NoteDisplayActivity extends AppCompatActivity {
                         + " ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in"
                         + " voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non"
                         + " proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_note_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_note_gps:
+                Intent mapActivity = new Intent(NoteDisplayActivity.this, MapsActivity.class);
+                startActivity(mapActivity);
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }

e887a39ba2035c5bbec97d310cd4c9214c5e2404
Yago González <yagogonzalezg@gmail.com>
2021-04-01 14:43:01 +0200

    chore: Remove newlines

--- app/src/main/java/com/github/steroidteam/todolist/view/ItemViewActivity.java
+++ app/src/main/java/com/github/steroidteam/todolist/view/ItemViewActivity.java
@@ -6,7 +6,6 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;

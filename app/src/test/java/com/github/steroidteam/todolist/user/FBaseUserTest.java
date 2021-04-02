6968ecdb46c270a8b707e56e7ed59eed07ef2888
Théo Damiani <theo.damiani@epfl.ch>
2021-04-02 17:32:23 +0200

    chore: spotless check on FBaseUserTest

--- app/src/test/java/com/github/steroidteam/todolist/user/FBaseUserTest.java
+++ app/src/test/java/com/github/steroidteam/todolist/user/FBaseUserTest.java
@@ -1,17 +1,18 @@
 package com.github.steroidteam.todolist.user;
 
-import androidx.annotation.NonNull;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
+import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
@@ -19,19 +20,12 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.MockitoRule;
 import org.mockito.quality.Strictness;
 
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-
 @RunWith(MockitoJUnitRunner.class)
 public class FBaseUserTest {
 
-    @Rule
-    public MockitoRule rule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
+    @Rule public MockitoRule rule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
 
-    @Mock
-    FirebaseAuth authMock;
+    @Mock FirebaseAuth authMock;
 
     FBaseUser fBaseUser;
 
@@ -50,33 +44,51 @@ public class FBaseUserTest {
     public void throwExceptionWhenNotLoggedIn() {
         when(authMock.getCurrentUser()).thenReturn(null);
 
-        assertThrows(User.UserLoginException.class, () -> {
-            fBaseUser.getUserId();
-        });
-        assertThrows(User.UserLoginException.class, () -> {
-            fBaseUser.getDisplayName();
-        });
-        assertThrows(User.UserLoginException.class, () -> {
-            fBaseUser.getEmail();
-        });
-        assertThrows(User.UserLoginException.class, () -> {
-            fBaseUser.isEmailVerified();
-        });
-        assertThrows(User.UserLoginException.class, () -> {
-            fBaseUser.updateDisplayName("", null);
-        });
-        assertThrows(User.UserLoginException.class, () -> {
-            fBaseUser.updateEmail("", null);
-        });
-        assertThrows(User.UserLoginException.class, () -> {
-            fBaseUser.sendEmailVerification(null);
-        });
-        assertThrows(User.UserLoginException.class, () -> {
-            fBaseUser.sendPasswordResetEmail(null);
-        });
-        assertThrows(User.UserLoginException.class, () -> {
-            fBaseUser.deleteUser(null);
-        });
+        assertThrows(
+                User.UserLoginException.class,
+                () -> {
+                    fBaseUser.getUserId();
+                });
+        assertThrows(
+                User.UserLoginException.class,
+                () -> {
+                    fBaseUser.getDisplayName();
+                });
+        assertThrows(
+                User.UserLoginException.class,
+                () -> {
+                    fBaseUser.getEmail();
+                });
+        assertThrows(
+                User.UserLoginException.class,
+                () -> {
+                    fBaseUser.isEmailVerified();
+                });
+        assertThrows(
+                User.UserLoginException.class,
+                () -> {
+                    fBaseUser.updateDisplayName("", null);
+                });
+        assertThrows(
+                User.UserLoginException.class,
+                () -> {
+                    fBaseUser.updateEmail("", null);
+                });
+        assertThrows(
+                User.UserLoginException.class,
+                () -> {
+                    fBaseUser.sendEmailVerification(null);
+                });
+        assertThrows(
+                User.UserLoginException.class,
+                () -> {
+                    fBaseUser.sendPasswordResetEmail(null);
+                });
+        assertThrows(
+                User.UserLoginException.class,
+                () -> {
+                    fBaseUser.deleteUser(null);
+                });
     }
 
     @Test
@@ -85,7 +97,7 @@ public class FBaseUserTest {
         when(authMock.getCurrentUser()).thenReturn(firebaseUserMock);
 
         Task<Void> taskMock = Mockito.mock(Task.class);
-        //when(firebaseUserMock.updateProfile(any())).thenReturn(taskMock);
+        // when(firebaseUserMock.updateProfile(any())).thenReturn(taskMock);
         when(firebaseUserMock.updateEmail(anyString())).thenReturn(taskMock);
         when(firebaseUserMock.sendEmailVerification()).thenReturn(taskMock);
         when(firebaseUserMock.updatePassword(anyString())).thenReturn(taskMock);
@@ -93,12 +105,13 @@ public class FBaseUserTest {
         when(authMock.sendPasswordResetEmail(anyString())).thenReturn(taskMock);
         when(firebaseUserMock.delete()).thenReturn(taskMock);
 
-        OnCompleteListener<Void> onComplete = new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                // Do nothing
-            }
-        };
+        OnCompleteListener<Void> onComplete =
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Do nothing
+                    }
+                };
 
         try {
             fBaseUser.getUserId();
@@ -121,7 +134,7 @@ public class FBaseUserTest {
             fail();
         }
         // FIXME : test fails because some android.text.TextUtils is not mocked
-        //fBaseUser.updateDisplayName("", null);
+        // fBaseUser.updateDisplayName("", null);
         try {
             fBaseUser.updateEmail("", onComplete);
         } catch (User.UserLoginException ule) {
@@ -147,6 +160,5 @@ public class FBaseUserTest {
         } catch (User.UserLoginException ule) {
             fail();
         }
-
     }
 }

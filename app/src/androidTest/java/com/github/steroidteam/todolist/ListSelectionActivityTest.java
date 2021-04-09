05070cdf8cae1d49e4fba65a2586ff8b9a9ae849
Théo Damiani <theo.damiani@epfl.ch>
2021-04-09 14:41:07 +0200

    test:

--- app/src/androidTest/java/com/github/steroidteam/todolist/ListSelectionActivityTest.java
+++ app/src/androidTest/java/com/github/steroidteam/todolist/ListSelectionActivityTest.java
@@ -1,18 +1,10 @@
 package com.github.steroidteam.todolist;
 
-import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.matcher.RootMatchers.isDialog;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.view.View;
 import android.widget.TextView;
@@ -60,27 +52,6 @@ public class ListSelectionActivityTest {
         intended(
                 Matchers.allOf(IntentMatchers.hasComponent(NoteSelectionActivity.class.getName())));
         Intents.release();
-    }
-
-    @Test
-    public void createTodoWorks() {
-
-        final String TODO_DESC = "A Todo!";
-
-        onView(withId(R.id.create_todo_button)).perform(click());
-
-        onView(withText("Enter the title of your to-do list")).check(matches(isDisplayed()));
-
-        onView(withId(R.id.alert_dialog_edit_text))
-                .inRoot(isDialog())
-                .perform(clearText(), typeText(TODO_DESC));
-        closeSoftKeyboard();
-        onView(withId(android.R.id.button1)).inRoot(isDialog()).perform(click());
-
-        onView(withText("Enter the title of your to-do list")).check(doesNotExist());
-
-        onView(withId(R.id.activity_list_selection_itemlist))
-                .check(matches(atPositionCheckText(0, TODO_DESC)));
     }
 
     public static Matcher<View> atPositionCheckText(

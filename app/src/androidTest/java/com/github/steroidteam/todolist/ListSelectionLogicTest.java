3a211e5d40e9aa43505f3c208442d9a7fa97face
Théo Damiani <theo.damiani@epfl.ch>
2021-04-10 12:24:26 +0200

    test: add scroll to position

--- app/src/androidTest/java/com/github/steroidteam/todolist/ListSelectionLogicTest.java
+++ app/src/androidTest/java/com/github/steroidteam/todolist/ListSelectionLogicTest.java
@@ -4,9 +4,12 @@ import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
 import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -31,7 +34,7 @@ import org.junit.runner.RunWith;
 public class ListSelectionLogicTest {
 
     @Test
-    public void createTodoWorks() {
+    public void renameTodoWorks() {
 
         Intent activity =
                 new Intent(
@@ -39,22 +42,27 @@ public class ListSelectionLogicTest {
 
         try (ActivityScenario<ListSelectionActivity> scenario = ActivityScenario.launch(activity)) {
 
-            final String TODO_DESC = "A Todo!";
+            final String TODO_DESC_2 = "Homework";
 
-            onView(withId(R.id.create_todo_button)).perform(click());
+            onView(withId(R.id.activity_list_selection_itemlist)).perform(scrollToPosition(0));
 
-            onView(withText("Enter the title of your to-do list")).check(matches(isDisplayed()));
+            onView(withId(R.id.activity_list_selection_itemlist))
+                    .perform(actionOnItemAtPosition(0, swipeRight()));
+
+            onView(withText("Rename your to-do list")).check(matches(isDisplayed()));
 
             onView(withId(R.id.alert_dialog_edit_text))
                     .inRoot(isDialog())
-                    .perform(clearText(), typeText(TODO_DESC));
+                    .perform(clearText(), typeText(TODO_DESC_2));
             closeSoftKeyboard();
             onView(withId(android.R.id.button1)).inRoot(isDialog()).perform(click());
 
-            onView(withText("Enter the title of your to-do list")).check(doesNotExist());
+            onView(withText("Rename your to-do list")).check(doesNotExist());
+
+            onView(withId(R.id.activity_list_selection_itemlist)).perform(scrollToPosition(0));
 
             onView(withId(R.id.activity_list_selection_itemlist))
-                    .check(matches(atPositionCheckText(0, TODO_DESC)));
+                    .check(matches(atPositionCheckText(0, TODO_DESC_2)));
         }
     }
 

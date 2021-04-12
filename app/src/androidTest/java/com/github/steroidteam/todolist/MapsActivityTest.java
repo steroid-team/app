9d1a43f82896c6ab72ef7c5dd4cc815577a246f2
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2021-04-12 23:22:21 +0200

    add test

---
+++ app/src/androidTest/java/com/github/steroidteam/todolist/MapsActivityTest.java
@@ -0,0 +1,23 @@
+package com.github.steroidteam.todolist;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.github.steroidteam.todolist.view.ListSelectionActivity;
+import com.github.steroidteam.todolist.view.NoteDisplayActivity;
+import com.github.steroidteam.todolist.view.NoteSelectionActivity;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MapsActivityTest {
+}

1a85b621065afddc8616217b7620559b6e99de55
Théo Damiani <theo.damiani@epfl.ch>
2021-04-12 20:00:59 +0200

    view: renameTodo

--- app/src/main/java/com/github/steroidteam/todolist/view/ListSelectionActivity.java
+++ app/src/main/java/com/github/steroidteam/todolist/view/ListSelectionActivity.java
@@ -122,7 +122,7 @@ public class ListSelectionActivity extends AppCompatActivity {
 
     public void renameTodo(UUID toDoListID, final int position) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Rename your to-do list");
+        builder.setMessage("Rename your to-do list");
 
         LayoutInflater inflater = this.getLayoutInflater();
         View dialog_input = inflater.inflate(R.layout.alert_dialog_input, null);

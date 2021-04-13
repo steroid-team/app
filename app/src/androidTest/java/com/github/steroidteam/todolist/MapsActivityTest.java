ed1985e920017b5c759dfa37520a3f79bc8153f8
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2021-04-13 10:43:13 +0200

    remove mapsTest 2

--- app/src/androidTest/java/com/github/steroidteam/todolist/MapsActivityTest.java
+++ app/src/androidTest/java/com/github/steroidteam/todolist/MapsActivityTest.java
@@ -1,20 +1,15 @@
 package com.github.steroidteam.todolist;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import com.github.steroidteam.todolist.view.MapsActivity;
-import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MapsActivityTest {
-    @Rule
-    public ActivityScenarioRule<MapsActivity> activityRule =
-            new ActivityScenarioRule<>(MapsActivity.class);
-
     /**
-     * @Test public void MarkerIsCorrectlyPlacedAtDefaultLocation() throws UiObjectNotFoundException
-     * { UiDevice device = UiDevice.getInstance(getInstrumentation()); UiObject marker =
-     * device.findObject(new UiSelector().descriptionContains("Sydney :)")); marker.click(); }
+     * @Rule public ActivityScenarioRule<MapsActivity> activityRule = new
+     * ActivityScenarioRule<>(MapsActivity.class); @Test public void
+     * MarkerIsCorrectlyPlacedAtDefaultLocation() throws UiObjectNotFoundException { UiDevice device
+     * = UiDevice.getInstance(getInstrumentation()); UiObject marker = device.findObject(new
+     * UiSelector().descriptionContains("Sydney :)")); marker.click(); }
      */
 }

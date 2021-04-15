263f2cb72a3c83be26057ebffa99e8b2b7a337ed
Théo Damiani <theo.damiani@epfl.ch>
2021-04-15 20:20:02 +0200

    view: add color on swipe

--- app/src/main/java/com/github/steroidteam/todolist/view/TodoTouchHelper.java
+++ app/src/main/java/com/github/steroidteam/todolist/view/TodoTouchHelper.java
@@ -1,8 +1,14 @@
 package com.github.steroidteam.todolist.view;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.View;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import com.github.steroidteam.todolist.R;
 import com.github.steroidteam.todolist.view.adapter.TodoCollectionAdapter;
 
 public class TodoTouchHelper extends ItemTouchHelper.SimpleCallback {
@@ -34,6 +40,49 @@ public class TodoTouchHelper extends ItemTouchHelper.SimpleCallback {
                 activity.renameTodo(
                         ((TodoCollectionAdapter.TodoHolder) viewHolder).getIdOfTodo(), position);
             }
+        }
+    }
+
+    @Override
+    public void onChildDraw(
+            Canvas c,
+            RecyclerView recyclerView,
+            RecyclerView.ViewHolder holder,
+            float dX,
+            float dY,
+            int actionState,
+            boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, holder, dX, dY, actionState, isCurrentlyActive);
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            View itemView = holder.itemView;
+
+            ColorDrawable colorToDisplay;
+
+            if (dX > 0) {
+                colorToDisplay =
+                        new ColorDrawable(
+                                ContextCompat.getColor(
+                                        itemView.getContext(), R.color.green_pastel));
+
+                colorToDisplay.setBounds(
+                        itemView.getLeft(),
+                        itemView.getTop(),
+                        (int) dX + 100,
+                        itemView.getBottom());
+            } else if (dX < 0) {
+                colorToDisplay =
+                        new ColorDrawable(
+                                ContextCompat.getColor(itemView.getContext(), R.color.red_pastel));
+                colorToDisplay.setBounds(
+                        itemView.getRight() + (int) dX - 100,
+                        itemView.getTop(),
+                        itemView.getRight(),
+                        itemView.getBottom());
+            } else {
+                colorToDisplay = new ColorDrawable(Color.WHITE);
+            }
+
+            colorToDisplay.draw(c);
         }
     }
 }

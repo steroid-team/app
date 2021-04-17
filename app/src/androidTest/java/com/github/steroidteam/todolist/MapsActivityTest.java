8a3d36c652aebf7f59447d4e16647c358f370ad3
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2021-04-17 12:58:40 +0200

    test only the searchBar

--- app/src/androidTest/java/com/github/steroidteam/todolist/MapsActivityTest.java
+++ app/src/androidTest/java/com/github/steroidteam/todolist/MapsActivityTest.java
@@ -4,10 +4,14 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.pressKey;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 import android.view.KeyEvent;
+import android.widget.AutoCompleteTextView;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -63,25 +67,14 @@ public class MapsActivityTest {
     }
 
     @Test
-    public void markerIsCorrectlyPlacedWhenACorrectLocationIsSearched() {
+    public void TextIsClearedAfterASearched() {
         final String CITY_NAME = "Lausanne";
 
         // Type a city name in the search bar
         onView(withId(R.id.sv_location))
                 .perform(
                         typeText(CITY_NAME), closeSoftKeyboard(), pressKey(KeyEvent.KEYCODE_ENTER));
-
-        // waitFor(3000);
-        UiDevice device = UiDevice.getInstance(getInstrumentation());
-        // The marker as the city name as title
-        UiSelector uiSelector = new UiSelector();
-        UiObject marker = device.findObject(uiSelector.descriptionContains(CITY_NAME));
-        try {
-            marker.click();
-            Assert.assertNotNull(marker);
-        } catch (UiObjectNotFoundException e) {
-            e.printStackTrace();
-        }
+        onView(isAssignableFrom(AutoCompleteTextView.class)).check(matches(withText("")));
         // onView(withContentDescription("Google Map")).check(matches(isDisplayed()));
     }
 

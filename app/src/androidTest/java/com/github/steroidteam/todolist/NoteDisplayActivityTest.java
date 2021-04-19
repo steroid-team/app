08bb3e3ed9a92fd067b97373a7b5c2c5d79309de
Vincent <vincentpellenc@gmail.com>
2021-04-19 09:15:01 +0200

    add non working test for file picker

--- app/src/androidTest/java/com/github/steroidteam/todolist/NoteDisplayActivityTest.java
+++ app/src/androidTest/java/com/github/steroidteam/todolist/NoteDisplayActivityTest.java
@@ -1,9 +1,21 @@
 package com.github.steroidteam.todolist;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Parcelable;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.GrantPermissionRule;
@@ -11,6 +23,12 @@ import com.github.steroidteam.todolist.view.NoteDisplayActivity;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteDisplayActivityTest {
@@ -25,5 +43,43 @@ public class NoteDisplayActivityTest {
     @Test
     public void saveTextWorks() {
         onView(withId(R.id.activity_notedisplay_save)).perform(click());
+    }
+
+    @Test
+    public void filePickingWorks() {
+        activityRule.getScenario().onActivity( activity -> {
+            savePickedImage(activity);
+            Instrumentation.ActivityResult res = createImageGallerySetResultStub(activity);
+            intending(hasAction(Intent.ACTION_CHOOSER)).respondWith(res);
+        });
+    }
+
+    public void savePickedImage(Activity activity) {
+        Bitmap bm = BitmapFactory.decodeResource(activity.getResources(), R.mipmap.asteroid_launcher_foreground);
+        File dir = activity.getExternalCacheDir();
+        File file = new File(dir.getPath(), "pickImageResult.jpeg");
+        FileOutputStream outStream;
+        try {
+            outStream = new FileOutputStream(file);
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+            outStream.flush();
+            outStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Instrumentation.ActivityResult createImageGallerySetResultStub(Activity activity) {
+        Bundle bundle = new Bundle();
+        ArrayList<Parcelable> parcels = new ArrayList<>();
+        Intent resultData = new Intent();
+        File dir = activity.getExternalCacheDir();
+        File file = new File(dir.getPath(), "pickImageResult.jpeg");
+        Uri uri = Uri.fromFile(file);
+        Parcelable parcelable1 = (Parcelable) uri;
+        parcels.add(parcelable1);
+        bundle.putParcelableArrayList(Intent.EXTRA_STREAM, parcels);
+        resultData.putExtras(bundle);
+        return new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
     }
 }

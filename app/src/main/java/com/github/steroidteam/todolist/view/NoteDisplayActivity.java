4d213c20c62c8538d3cfa9f16fdb489a8fa9c654
Vincent <vincentpellenc@gmail.com>
2021-04-21 21:34:01 +0200

    perform spotlessApply

--- app/src/main/java/com/github/steroidteam/todolist/view/NoteDisplayActivity.java
+++ app/src/main/java/com/github/steroidteam/todolist/view/NoteDisplayActivity.java
@@ -3,7 +3,6 @@ package com.github.steroidteam.todolist.view;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
-
 import com.github.steroidteam.todolist.R;
 import java.io.InputStream;
 

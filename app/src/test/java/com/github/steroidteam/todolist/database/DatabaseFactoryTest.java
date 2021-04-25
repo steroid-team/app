2d7b06fb9e30cb7209fd700bc3d8e3ad8de0ee24
she-reds <sydney.hauke@heig-vd.ch>
2021-04-25 17:39:15 +0200

    Add tests for DatabaseFactory.

---
+++ app/src/test/java/com/github/steroidteam/todolist/database/DatabaseFactoryTest.java
@@ -0,0 +1,22 @@
+package com.github.steroidteam.todolist.database;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DatabaseFactoryTest {
+
+    @Mock
+    Database databaseMock;
+
+    @Test
+    public void returnSettedCustomDatabase() {
+        DatabaseFactory.setCustomDatabase(databaseMock);
+        assertEquals(databaseMock, DatabaseFactory.getDb());
+    }
+}

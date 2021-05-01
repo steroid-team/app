182ed599d6751f0a26411fd37ef1e5e5ce520703
Théo Damiani <theo.damiani@epfl.ch>
2021-05-01 19:56:40 +0200

    spotless

--- app/src/main/java/com/github/steroidteam/todolist/model/TodoRepository.java
+++ app/src/main/java/com/github/steroidteam/todolist/model/TodoRepository.java
@@ -48,7 +48,8 @@ public class TodoRepository {
     }
 
     public void setTaskDone(int index, boolean isDone) {
-        this.database.setTaskDone(todoListID, index, isDone)
+        this.database
+                .setTaskDone(todoListID, index, isDone)
                 .thenCompose(task -> this.database.getTodoList(todoListID))
                 .thenAccept(this.oneTodoList::setValue);
     }

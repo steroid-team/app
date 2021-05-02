2974259e43e87794d90b9dc50e24e4d82d1665c4
Vincent <vincentpellenc@gmail.com>
2021-05-02 19:47:54 +0200

    add toString and equals for Note data class

--- app/src/main/java/com/github/steroidteam/todolist/model/notes/Note.java
+++ app/src/main/java/com/github/steroidteam/todolist/model/notes/Note.java
@@ -1,6 +1,8 @@
 package com.github.steroidteam.todolist.model.notes;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.google.android.gms.maps.model.LatLng;
 import java.util.UUID;
 
@@ -47,5 +49,18 @@ public class Note {
 
     public void setLatLng(@NonNull LatLng latLng) {
         this.latLng = latLng;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{\n" + "title='" + title + "',\n" + "content='" + content + "',\n" + "coordinates='" + latLng.toString() + "',\n}";
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return note.getId().equals(this.getId());
     }
 }

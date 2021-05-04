a1f98a7d4f99a4e10d765090430301869ca65b31
Yago González <yagogonzalezg@gmail.com>
2021-05-04 21:16:35 +0200

    chore: Rename constructor test more semantically

--- app/src/test/java/com/github/steroidteam/todolist/model/todo/TaskTest.java
+++ app/src/test/java/com/github/steroidteam/todolist/model/todo/TaskTest.java
@@ -1,13 +1,17 @@
 package com.github.steroidteam.todolist.model.todo;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 public class TaskTest {
 
     @Test
-    public void taskCorrectlyThrowsException() {
+    public void taskRejectsNullArguments() {
         assertThrows(
                 IllegalArgumentException.class,
                 () -> {

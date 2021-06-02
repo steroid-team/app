5f1a0564a06715446e71a10292a61cae0b2dc6a1
Yago González <yagogonzalezg@gmail.com>
2021-06-02 19:28:16 +0200

    views: Make 1-week task group category more explicit

--- app/src/main/java/com/github/steroidteam/todolist/view/adapter/ParentTaskAdapter.java
+++ app/src/main/java/com/github/steroidteam/todolist/view/adapter/ParentTaskAdapter.java
@@ -39,7 +39,7 @@ public class ParentTaskAdapter extends RecyclerView.Adapter<ParentTaskAdapter.Pa
         dateCategoryList.add("Past due");
         dateCategoryList.add("Today");
         dateCategoryList.add("Tomorrow");
-        dateCategoryList.add("Next 5 days after Tomorrow");
+        dateCategoryList.add("Next 7 days");
         dateCategoryList.add("Later");
         dateCategoryList.add("Unknown");
         dateCategoryList.add("Done");

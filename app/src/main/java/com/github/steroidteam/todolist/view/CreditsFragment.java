fc53cc884c3cdbd43ee1b0fd33475b373ce3fc55
Théo Damiani <theo.damiani@epfl.ch>
2021-06-04 17:54:31 +0200

    credits placehilder

---
+++ app/src/main/java/com/github/steroidteam/todolist/view/CreditsFragment.java
@@ -0,0 +1,33 @@
+package com.github.steroidteam.todolist.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.github.steroidteam.todolist.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CreditsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View root = inflater.inflate(R.layout.fragment_credits, container, false);
+
+        TextView textView = root.findViewById(R.id.credits_body);
+
+        textView.setText("Add Credits");
+
+        return root;
+    }
+}

a53aaea5a51bef8b74003c2bfed72563dda5f6b3
lgraziano <leandro.graziano@epfl.ch>
2021-06-04 20:45:08 +0200

    spotlessApply

--- app/src/main/java/com/github/steroidteam/todolist/model/todo/Tag.java
+++ app/src/main/java/com/github/steroidteam/todolist/model/todo/Tag.java
@@ -47,7 +47,7 @@ public class Tag {
 
     @Override
     public String toString() {
-        return ("Tag{" + body + ", " + uuid.toString() + "}");
+        return ("Tag{" + body + "}");
     }
 
     @Override
